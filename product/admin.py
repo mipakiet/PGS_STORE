bb27307acab4393688b95d6c31806153c4238da1
Mikolaj Pakiet (PGS Software) <mipakiet@pgs-soft.com>
2022-01-13 08:56:42 +0100

    better admin view for CartItem

--- product/admin.py
+++ product/admin.py
@@ -26,8 +26,37 @@ class ProductAdmin(admin.ModelAdmin):
         return mark_safe(f'<img src="{obj.image.url}" width="150" height="100" />')
 
 
+@admin.register(CartItem)
+class CartItem(admin.ModelAdmin):
+    list_display = (
+        "product",
+        "quantity",
+        "user",
+        "price_for_one",
+        "price_for_all",
+        "image_thumbnail",
+    )
+    actions = ["close"]
+
+    def price_for_one(self, obj):
+        return obj.product.price
+
+    def price_for_all(self, obj):
+        return obj.quantity * obj.product.price
+
+    def image_thumbnail(self, obj):
+        return mark_safe(
+            f'<img src="{obj.product.image.url}" width="150" height="100" />'
+        )
+
+    def close(self, request, queryset):
+        for obj in queryset:
+            obj.product.quantity -= obj.quantity
+            obj.product.save()
+            obj.delete()
+
+
 admin.site.register(Producer)
 admin.site.register(Category)
 admin.site.register(Computer)
 admin.site.register(City)
-admin.site.register(CartItem)
