9979e3eec81db3c7d9c9bd384d4fd5083290eda8
Mikolaj Pakiet (PGS Software) <mipakiet@xld006.pgs-soft.com>
2022-11-28 10:21:25 +0000

    set up

--- PGS_store/settings.py
+++ PGS_store/settings.py
@@ -102,19 +102,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-if SERVER:
-    STATIC_URL = "/static/"
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-    MEDIA_URL = "/media/"
-    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-else:
-    STATIC_URL = "/static/"
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-    MEDIA_URL = "/media/"
-    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
